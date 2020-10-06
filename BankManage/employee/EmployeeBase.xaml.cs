414f31d5d462232b7144128a385136f9a37160a6
craigxiang <31474900+craigxiang@users.noreply.github.com>
2020-10-06 00:38:01 -0400

    Bank Management Source Code
    Bank Management Source Code

---
+++ BankManage/employee/EmployeeBase.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BankManage.employee
+{
+    /// <summary>
+    /// EmployeeBase.xaml 的交互逻辑
+    /// </summary>
+    public partial class EmployeeBase : Page
+    {
+
+        BankEntities context = new BankEntities();
+        public EmployeeBase()
+        {
+          
+            InitializeComponent();
+            this.Unloaded += EmployeeBase_Unloaded;
+            var q = from t in context.EmployeeInfo
+                    select t;
+            dataGrid1.ItemsSource = q.ToList();
+            
+        }
+       void EmployeeBase_Unloaded(object sender, RoutedEventArgs e)
+        {
+            context.Dispose();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                context.SaveChanges();
+                MessageBox.Show("保存成功！");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "保存失败");
+            }
+        }
+
+        
+    }
+}
---
+++ BankManage/query/CustomQuery.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace BankManage.query
+{
+    /// <summary>
+    /// DayQuery.xaml 的交互逻辑
+    /// 当日汇总
+    /// </summary>
+    public partial class CustomQuery : Page
+    {
+        BankEntities context = new BankEntities();
+        public CustomQuery()
+        {
+            InitializeComponent();
+            this.Unloaded += CustomQuery_Unloaded;
+            
+        }
+        void CustomQuery_Unloaded(object sender, RoutedEventArgs e)
+        {
+            context.Dispose();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var query = from t in context.AccountInfo
+                        where t.IdCard == txtID.Text
+                        select t;
+                    
+            datagrid1.ItemsSource = query.ToList();
+        }
+
+       
+    }
+}
